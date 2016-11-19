0a33aa750813f528c1122430a800e3baddf83136
oresths <orestis@ee.auth.gr>
2016-11-19 21:34:14 +0200

    Create streamer from image files

--- tracker/src/dsst_test.cpp
+++ tracker/src/dsst_test.cpp
@@ -1,54 +1,39 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
 
-#include <sstream>
-
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
 int main(int argc, char **argv)
 {
 
-  ros::init(argc, argv, "talker");
+  ros::init(argc, argv, "stream_n_track");
 
-  ros::NodeHandle n;
+  ros::NodeHandle nh;
 
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  image_transport::ImageTransport it(nh);
+  image_transport::Publisher pub = it.advertise("/camera/image_raw", 1);
 
-  ros::Rate loop_rate(10);
 
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
-  int count = 0;
-  while (ros::ok())
-  {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
-    std_msgs::String msg;
+  cv::VideoCapture cap("/mnt/hgfs/Vision/Dataset UAV123/UAV123/data_seq/UAV123/bike1/%06d.jpg");
+  // Check if video device can be opened with the given index
+  if(!cap.isOpened()) return 1;
 
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
+  cv::Mat frame;
+  sensor_msgs::ImagePtr msg;
 
-    ROS_INFO("%s", msg.data.c_str());
+  ros::Rate loop_rate(25);
+  while (ros::ok()) {
+    cap >> frame;
 
-    /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
-     */
-    chatter_pub.publish(msg);
-
-    ros::spinOnce();
+    // Check if grabbed frame is actually full with some content
+    if(!frame.empty()) {
+      msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+      pub.publish(msg);
+    }
 
     loop_rate.sleep();
-    ++count;
   }
-
 
   return 0;
 }
